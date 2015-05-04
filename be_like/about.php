e0b073f0cc9c718e1031f29b92a9f478317b19b7
mambax7 <mambax7@gmail.com>
2015-05-04 04:20:26 -0400

    Original files

---
+++ be_like/about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+
+<script type="text/javascript">
+</script>
+<STYLE TYPE="text/css">
+<!--
+.td{
+	margin: 0px;
+}
+td.gray {
+	background-image: url(images/3.gif);
+	background-repeat: no-repeat;
+	padding: 0pt 0pt 0pt 0pt;
+	border-style: none;
+	border-width: 0px;
+	vertical-align : top; 
+	text-align: left;
+}
+graytext{
+	padding: 5pt 5pt 5pt 10pt;
+}
+td.white {
+	background-image: url(images/4.gif);
+	background-repeat: no-repeat;
+	border-style: none;
+	border-width: 0px;
+	vertical-align : middle; 
+	text-align: center;
+}
+a {color: #000000;; text-decoration: none;  background-color: transparent;}
+a:hover {color: #FF0000;}
+-->
+</STYLE> 
+</head>
+<body bgcolor="#336699">
+<br /><br /><br /><br /><br /><center>
+<table cellspacing="0" cellpadding="0" width="511">
+<tr><!-- history.go(-1); -->
+<td colspan="2" width="511" height="23"><img src="images/1.gif" width="511" height="23" onclick="history.go(-1);"></td>
+</tr><tr>
+<td width="205" height="62"><img src="images/2.gif" width="205" height="62"></td>
+<td rowspan="2" width="306" height="308" class="white" >
+be_like designed by xoops-modules project.<br /><br />
+<a href="http://xoops-modules.sourceforge.jp/" target="_blank">Project Page</a><br /><br />
+<a href="http://sourceforge.jp/projects/xoops-modules/" target="_blank">Sourceforge.jp Page</a><br /><br />
+<a href="http://sourceforge.jp/" target="_blank"><img src="http://sourceforge.jp/sflogo.php?group_id=757" border="0"></a><br /><br />
+The target browser of this theme only IE.(sorry)<br /><br />
+Please click window tab if you want back!
+</td>
+</tr>
+<tr>
+<td class="gray" width="205" height="246" align="left" valign="top">
+<dl>
+<dt><b>&nbsp;&nbsp;Plathome</b>
+<dd><a href="http://www.xoops.org/" target="_blank">XOOPS 2.0.5</a>
+<dt><b>&nbsp;&nbsp;RemoteAddress</b>
+<dd>219.34.60.44<dt><b>&nbsp;&nbsp;Accept-Language</b>
+<dd><dt><b>&nbsp;&nbsp;Accept-Encoding</b>
+<dd>gzip, deflate<dt><b>&nbsp;&nbsp;Server-Protocol</b>
+<dd>HTTP/1.1</dl>
+<!--
+<b>&nbsp;&nbsp;Plathome</b><br>&nbsp&nbsp;&nbsp;&nbsp;&nbsp;XOOPS 2.0.5<br /><br />
+<b>&nbsp;&nbsp;RemoteAddress</b><br>&nbsp&nbsp;&nbsp;&nbsp;&nbsp;219.34.60.44<br /><br />
+<b>&nbsp;&nbsp;Accept-Language</b><br>&nbsp&nbsp;&nbsp;&nbsp;&nbsp;<br /><br />
+<b>&nbsp;&nbsp;Accept-Encoding</b><br>&nbsp&nbsp;&nbsp;&nbsp;&nbsp;gzip, deflate<br />
+-->
+</td>
+</tr>
+</table>
+
+</center>
+</body>
+</html>
